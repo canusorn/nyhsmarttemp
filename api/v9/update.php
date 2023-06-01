932eb59a688602afa63e0b10bfce2d58cb42537c
canusorn <39732595+canusorn@users.noreply.github.com>
2023-06-01 20:04:01 +0700

    del other project

--- api/v9/update.php
+++ api/v9/update.php
@@ -33,48 +33,38 @@ if (!empty($_POST) && isset($_POST['esp_id']) && isset($_POST['user_id'])) {
     // check esp id with user id
     // if ($esp_id->user_id == $_POST['user_id']) {
 
-        // update lastupdate from device
-        Esp_ID::lastTimeUpdate($conn, $_POST['esp_id'], $dateTime->format('Y-m-d H:i:s'));
+    // update lastupdate from device
+    Esp_ID::lastTimeUpdate($conn, $_POST['esp_id'], $dateTime->format('Y-m-d H:i:s'));
 
-        // update sec data for each project
-        foreach ($_POST['data'] as $data) {
-            if ($data['project_id'] == 0) {
-                require("0custom.php");
-            } else if ($data['project_id'] == 1) {
-                require("1acmeter.php");
-            } else if ($data['project_id'] == 2) {
-                require("2pmmeter.php");
-            } else if ($data['project_id'] == 3) {
-                require("3dcmeter.php");
-            } else if ($data['project_id'] == 4) {
-                require("4dht.php");
-            } else if ($data['project_id'] == 6) {
-                require("6acmeter3p.php");
-            }
-            // echo '<br>line:';
-            // var_dump($line_sent);
-            // echo '<br>payload:';
-            // var_dump($payload);
-            // echo '<br>';
+    // update sec data for each project
+    foreach ($_POST['data'] as $data) {
+        if ($data['project_id'] == 4) {
+            require("4dht.php");
         }
+        // echo '<br>line:';
+        // var_dump($line_sent);
+        // echo '<br>payload:';
+        // var_dump($payload);
+        // echo '<br>';
+    }
 
-        if ($esp_id->need_ota) echo "&32765";   // if need ota update from server
+    if ($esp_id->need_ota) echo "&32765";   // if need ota update from server
 
 
-        // full echo
-        // echo "32765&" . (json_encode($payload));
+    // full echo
+    // echo "32765&" . (json_encode($payload));
 
-        if (!empty($payload["error"])) {
-            $datalog = $dateTime->format('Y-m-d H:i:s') . ", EspID:" . $_POST['esp_id'] . "\n";
-            foreach ($payload["error"] as  $error) {
-                $datalog .= "-" . $error . "\n";
-            }
-            file_put_contents('error.log', $datalog . "\n", FILE_APPEND);
-            // file_put_contents('var_dump.log', $datalog);
+    if (!empty($payload["error"])) {
+        $datalog = $dateTime->format('Y-m-d H:i:s') . ", EspID:" . $_POST['esp_id'] . "\n";
+        foreach ($payload["error"] as  $error) {
+            $datalog .= "-" . $error . "\n";
         }
+        file_put_contents('error.log', $datalog . "\n", FILE_APPEND);
+        // file_put_contents('var_dump.log', $datalog);
+    }
 
-        // notify line sent
-        require 'line_noti.php';
+    // notify line sent
+    require 'line_noti.php';
     // } else {
     //     echo ("0" . "&" . "wrong user");
     // }
